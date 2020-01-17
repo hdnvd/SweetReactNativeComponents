498e28255621881315a743afd73c9b7b2ebf3951
Hadi AmirNahavandi <Hadi.nahavandi2010@gmail.com>
2020-01-17 13:47:33 +0330

    fix touch bug when is not editable.

--- src/TextBox.js
+++ src/TextBox.js
@@ -10,22 +10,28 @@ import {Text, View, TextInput, TouchableOpacity, Dimensions, StyleSheet} from 'r
 export default class TextBox extends Component<{}> {
     render() {
         const BorderColor=this.props.BorderColor;
+        const txtInput=<TextInput keyboardType={this.props.keyboardType} placeholder=''  underlineColorAndroid={'transparent'}
+                                  style={this.props.textStyle!=null?this.props.textStyle:[Styles.input,{borderColor:BorderColor}]}
+                                  ref = {(input) => this.input = input}
+                                  onChangeText={(text) =>{this.props.onChangeText(text)} }
+                                  allowFontScaling={false}
+                                  scrollEnabled={false}
+                                  placeholder={this.props.placeholder===undefined?this.props.title:this.props.placeholder}
+                                  value={(this.props.value===undefined||this.props.value===null)?'':this.props.value} {...this.props} />;
         return (
             <View>
                 <Text style={this.props.labelStyle!=null?this.props.labelStyle:Styles.inputLabel}>{this.props.title}</Text>
+                {this.props.editable===false &&
+                txtInput
+                }
+                {this.props.editable!==false &&
                 <TouchableOpacity
                     activeOpacity={1}
                     onPress={()=>this.input.focus()}>
                     <View pointerEvents="none">
-                <TextInput keyboardType={this.props.keyboardType} placeholder=''  underlineColorAndroid={'transparent'}
-                           style={this.props.textStyle!=null?this.props.textStyle:[Styles.input,{borderColor:BorderColor}]}
-                           ref = {(input) => this.input = input}
-                           onChangeText={(text) =>{this.props.onChangeText(text)} }
-                           allowFontScaling={false}
-                           scrollEnabled={false}
-                           placeholder={this.props.placeholder===undefined?this.props.title:this.props.placeholder}
-                           value={(this.props.value===undefined||this.props.value===null)?'':this.props.value} {...this.props} />
-                </View></TouchableOpacity>
+                        {txtInput}
+                    </View></TouchableOpacity>
+                }
             </View>);
     }
 }
